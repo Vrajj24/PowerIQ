8c730135d0edbb5cf4b1ee73fb992d5859d13f28
Vrajj24 <119157546+Vrajj24@users.noreply.github.com>
2026-07-10 16:24:00 +0530

    Add index.ts

---
+++ frontend/src/services/index.ts
@@ -0,0 +1,7 @@
+export * from './api';
+export * from './authService';
+export * from './deviceService';
+export * from './alertService';
+export * from './dashboardService';
+export * from './analyticsService';
+export * from './settingsService';
